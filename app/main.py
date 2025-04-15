9b218fedd1658dc0190a63e90fcf21795a978605
timothyobosi <timothyobosi8@gmail.com>
2025-04-15 15:36:36 +0300

    Added Post, Update and delete Endpoints

--- app/main.py
+++ app/main.py
@@ -22,4 +22,25 @@ class Movie(BaseModel):
 async def index():
    return fake_movie_db
 
+@app.post('/', status_code=201)
+async def add_movie(payload: Movie):
+     movie = payload.dict()
+     fake_movie_db.append(movie)
+     return {'d' : len(fake_movie_db) - 1}
 
+@app.put('/{id}')
+async def update_movie(id: int, payload: Movie):
+     movie = payload.dict()
+     movies_length = len(fake_movie_db)
+     if 0 <= id <= movies_length:
+          fake_movie_db[id] = movie
+          return None
+     raise HTTPException(status_code=404, detail="Movie with given id not found")
+
+@app.delete('/{id}')
+async def delete_movie(id: int):
+     movies_length = len(fake_movie_db)
+     if 0 <= id <= movies_length:
+          del fake_movie_db[id]
+          return None
+     raise HTTPException(status_code=404, detail="Movie with given id is not found")
